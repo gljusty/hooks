b81922954a36495dea98facc6adab311421cc08e
Bryan Stapleton <90147833+gljusty@users.noreply.github.com>
2022-12-04 23:06:51 -0700

    Update useleaderline.ts

--- useleaderline.ts
+++ useleaderline.ts
@@ -1,34 +1,35 @@
-import LeaderLine, { Options } from "leader-line-new"
-import React, { useEffect, useState } from "react"
+import LeaderLine, { Options } from 'leader-line-new'
+import React, { useEffect, useState } from 'react'
 
 const useLeaderLine = (
-  c?: Options
+    c?: Options
 ): [
-  LeaderLine[],
-  () => { (): void; generate(c?: Options): void },
-  React.Dispatch<React.SetStateAction<LeaderLine.Options | undefined>>
+    LeaderLine[],
+    () => { (): void; generate(c?: Options): void },
+    React.Dispatch<React.SetStateAction<LeaderLine.Options | undefined>>
 ] => {
-  const [config, updateConfig] = useState<Options | undefined>()
-  const [lines, updateLines] = useState<LeaderLine[]>([])
+    const [config, updateConfig] = useState<Options | undefined>()
+    const [lines, updateLines] = useState<LeaderLine[]>([])
 
-  const leaderLineGenerator = () => {
-    const generator = () => {}
-    generator.generate = (_?: Options) => {
-      try {
-        const line = new LeaderLine(_ || config!)
-        updateLines(prev => [...prev, line])
-      } catch {
-        //do nothing
-      }
+    const leaderLineGenerator = () => {
+        const generator = () => {}
+        generator.generate = (_?: Options) => {
+            try {
+                const line = new LeaderLine(_ || config!)
+                updateLines((prev) => [...prev, line])
+            } catch {
+                //do nothing
+            }
+        }
+        return generator
     }
-    return generator
-  }
 
-  useEffect(() => {
-    c ? updateConfig(c) : console.log("!c")
-  }, [c])
+    useEffect(() => {
+        c ? updateConfig(c) : console.log('!c')
+    }, [c])
 
-  return [lines, leaderLineGenerator, updateConfig]
+    return [lines, leaderLineGenerator, updateConfig]
 }
 
 export default useLeaderLine
+
